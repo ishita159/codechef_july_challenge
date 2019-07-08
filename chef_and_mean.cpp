f1e595840c8a35ce03bed55a5d13389763f1a957
Ishita Singh <42881855+ishita159@users.noreply.github.com>
2019-07-09 00:54:43 +0530

    Add files via upload

--- chef_and_mean.cpp
+++ chef_and_mean.cpp
@@ -1,14 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+int MAX = 100008;
 
-long long sum(vector<int> &array, int n)
+int binarySearch(int *arr, int l, int r, double x)
 {
-    long long sum = 0;
-    for (int i = 0; i < n; i++)
+    if (r >= l)
     {
-        sum += array[i];
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+        return binarySearch(arr, mid + 1, r, x);
     }
-    return sum;
+    return -1;
+}
+int search(int *arr, int n, double x)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        if (arr[i] == x)
+            return i+1;
+    return -1;
 }
 
 int main()
@@ -17,42 +30,18 @@ int main()
     scanf("%d", &t);
     while (t--)
     {
-        int n, flag = 0, j = 0,val;
-        vector<int> chef_coins, coin;
-            scanf("%d", &n);
+        int n, array[MAX];
+        double sum;
+        scanf("%d", &n);
         for (int i = 0; i < n; i++)
         {
-            scanf("%d",&val);
-            chef_coins.push_back(val);
+            scanf("%d", &array[i]);
+            sum += array[i];
         }
-
-        long long sum1 = sum(chef_coins, n);
-        float mean_original = sum1 / (float)n;
-
-        float mean_new = 0;
-        n = n - 1;
-        for (int i = 0; i < n; i++)
-        {
-
-            mean_new = (sum1 - chef_coins[i]) / (float)n;
-
-            if (mean_new == mean_original)
-            {
-                flag++;
-                coin.push_back(i);
-            
-            }
-        }
-        if (flag == 0)
-        {
-            printf("Impossible\n");
-        }
-        else
-        {
-
-            sort(coin.begin(), coin.end());
-            printf("%d\n", coin[0] + 1);
-        }
+        sum = sum /n;
+        int result = binarySearch(array,0, n-1, sum);
+        (result == -1) ? printf("Impossible\n")
+                       : printf("%d\n",
+                                result);
     }
-    return 0;
 }
